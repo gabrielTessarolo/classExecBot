a878f6269d372c06890cce993165b4d4cbaa1955
gabrielTessarolo <gabriel.tessarolo.h@gmail.com>
2024-11-11 08:04:30 -0300

    Verificação de meses múltiplos e formatação do output

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.options import Options
 from time import sleep
 from datetime import datetime
 
+monthsPassed = int(input("Verificar quantos meses anteriores? "))
 driver = webdriver.Chrome()
 
 driver.get("https://siap.educacao.go.gov.br/login.aspx?ReturnUrl=%2fdefault.aspx")
@@ -19,7 +20,8 @@ def clickSeq(*args, slp=0.1):
 
 def login():
     # Coloque os dados
-    userData = {"login": input("Login SIAP: "), "password": input("Senha SIAP: ")}
+    userData = {"login": "89034058115", "password": "o9979EqH"}
+    # userData = {"login": input("Login SIAP: "), "password": input("Senha SIAP: ")}
     seletorX('//*[@id="txtLogin"]').send_keys(userData["login"])
     seletorX('//*[@id="txtSenha"]').send_keys(userData["password"])
 
@@ -30,6 +32,7 @@ def login():
     seletorX('//*[@id="btnLogon"]').click()
 
 diaMes = datetime.now().day
+mesAtual = datetime.now().month
 
 def executeClass(td):
     td.click()
@@ -47,55 +50,64 @@ def executeFreq(td):
     seletorX('//*[@id="cphFuncionalidade_btnAlterar"]').click()
 
 def acessExec(idx):
-    for line in range(1, 6):
-        for col in range([3,5][idx==1], 6):
-            try:  
-                actDay = int(seletorX(f'//*[@id="cphFuncionalidade_cphCampos_CalendarioMensal"]/div/table/tbody/tr[{line}]/td[{col}]/div').text)
-            except:
-                continue
-            # Verifica se o dia não é maior
-            if actDay < diaMes:
-                actTd = seletorX(f'//*[@id="cphFuncionalidade_cphCampos_CalendarioMensal"]/div/table/tbody/tr[{line}]/td[{col}]')
-                # Verifica se o dia atual de fato tem aula (se não é feriado ou outra coisa)
-                if ('dialog' in actTd.get_attribute('class').split() and 'letivo' in actTd.get_attribute('class').split()):
-                    # Verifica se está planejado
-                    if actTd.get_attribute('data-planejado')=="True":
-                        # Verifica se já foi executado
-                        if actTd.get_attribute('data-executado')=="False":
-                            print(f'Aula do dia {actDay} \033[34melegível para ser executada\033[m.')
-                            executeClass(actTd)
-                    else: 
-                        print(f'Aula do dia {actDay} \033[31mnão planejada\033[m.')
+    for mes in range(mesAtual-monthsPassed, mesAtual+1):
+        clickSeq('//*[@id="selectMesCalendarioMensal"]', f'//*[@id="selectMesCalendarioMensal"]/option[{mes}]')
+        for line in range(1, 6):
+            for col in range([3,5][idx==1], 6):
+                try:  
+                    actDay = int(seletorX(f'//*[@id="cphFuncionalidade_cphCampos_CalendarioMensal"]/div/table/tbody/tr[{line}]/td[{col}]/div').text)
+                except:
+                    continue
+                # Verifica se o dia não é maior
+                if (mes==mesAtual and actDay < diaMes) or mes<mesAtual:
+                    # print(f"\033[1;35mPASSOU AQUI PELO MENOS - dia {actDay}\033[m")
+                    actTd = seletorX(f'//*[@id="cphFuncionalidade_cphCampos_CalendarioMensal"]/div/table/tbody/tr[{line}]/td[{col}]')
+                    # Verifica se o dia atual de fato tem aula (se não é feriado ou outra coisa)
+                    if ('dialog' in actTd.get_attribute('class').split() and 'letivo' in actTd.get_attribute('class').split()):
+                        # Verifica se está planejado
+                        if actTd.get_attribute('data-planejado')=="True":
+                            # Verifica se já foi executado
+                            if actTd.get_attribute('data-executado')=="False":
+                                print(f'Aula do dia {actDay}/{mesAtual} \033[34melegível para ser executada\033[m.')
+                                executeClass(actTd)
+                            else:
+                                print(f'Aula do dia {actDay}/{mesAtual} \033[32mjá executada\033[m.')
+                        else: 
+                            print(f'Aula do dia {actDay}/{mesAtual} \033[31mnão planejada\033[m.')
 
 def accessFreq(idx):
     seletorX('//*[@id="cphFuncionalidade_cphCampos_BtnFrequencia"]').click()
     sleep(1)
 
-    for line in range(1, 6):
-        for col in range([3,5][idx==1], 6):
-            try:  
-                actDay = int(seletorX(f'//*[@id="cphFuncionalidade_cphCampos_CalendarioMensal"]/div/table/tbody/tr[{line}]/td[{col}]/div').text)
-            except:
-                continue
-            # Verifica se o dia não é maior
-            if actDay < diaMes:
-                actTd = seletorX(f'//*[@id="cphFuncionalidade_cphCampos_CalendarioMensal"]/div/table/tbody/tr[{line}]/td[{col}]')
-                # Verifica se o dia atual de fato tem aula (se não é feriado ou outra coisa)
-                if ('dialog' in actTd.get_attribute('class').split() and 'letivo' in actTd.get_attribute('class').split()):
-                    # Verifica se já a frequência já foi executada
-                    if actTd.get_attribute('data-executado')=="False":
-                        print(f'Frequência do dia {actDay} \033[34melegível para ser executada\033[m.')
-                        executeFreq(actTd)
+    for mes in range(mesAtual-monthsPassed, mesAtual+1):
+        clickSeq('//*[@id="selectMesCalendarioMensal"]', f'//*[@id="selectMesCalendarioMensal"]/option[{mes}]')
+        for line in range(1, 6):
+            for col in range([3,5][idx==1], 6):
+                try:  
+                    actDay = int(seletorX(f'//*[@id="cphFuncionalidade_cphCampos_CalendarioMensal"]/div/table/tbody/tr[{line}]/td[{col}]/div').text)
+                except:
+                    continue
+                # Verifica se o dia não é maior
+                if (mes==mesAtual and actDay < diaMes) or mes<mesAtual:
+                    actTd = seletorX(f'//*[@id="cphFuncionalidade_cphCampos_CalendarioMensal"]/div/table/tbody/tr[{line}]/td[{col}]')
+                    # Verifica se o dia atual de fato tem aula (se não é feriado ou outra coisa)
+                    if ('dialog' in actTd.get_attribute('class').split() and 'letivo' in actTd.get_attribute('class').split()):
+                        # Verifica se já a frequência já foi executada
+                        if actTd.get_attribute('data-executado')=="False":
+                            print(f'Frequência do dia {actDay}/{mesAtual} \033[34melegível para ser executada\033[m.')
+                            executeFreq(actTd)
+                        else:
+                            print(f'Frequência do dia {actDay}/{mesAtual} \033[32mjá executada\033[m.')
 
 login()
 
 for i in range(1, 5):
     driver.get('https://siap.educacao.go.gov.br/DiarioEscolarListagem.aspx')
-
     clickSeq('//*[@id="cphFuncionalidade_cphCampos_ddlComposicao"]', '//*[@id="cphFuncionalidade_cphCampos_ddlComposicao"]/option[3]',
              '//*[@id="cphFuncionalidade_cphCampos_ddlBimestre"]', '//*[@id="cphFuncionalidade_cphCampos_ddlBimestre"]/option[4]',
              '//*[@id="cphFuncionalidade_cphCampos_ddlTurno"]', '//*[@id="cphFuncionalidade_cphCampos_ddlTurno"]/option[2]',
              '//*[@id="cphFuncionalidade_btnListar"]', slp=0.2)
+    print('\033[33mTurma '+seletorX(f'//*[@id="cphFuncionalidade_gdvListagem"]/tbody/tr[{i+1}]/td[8]').text+'\033[m')
     clickSeq(f'//*[@id="cphFuncionalidade_gdvListagem"]/tbody/tr[{i+1}]', '//*[@id="cphFuncionalidade_btnAuxiliar1"]', slp=2)
     
     acessExec(i)
